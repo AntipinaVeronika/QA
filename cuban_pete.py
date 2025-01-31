7b14c1c0fe75826889e42daccde349f2477cb6e6
AntipinaVeronika <144445202+AntipinaVeronika@users.noreply.github.com>
2025-01-31 23:49:26 +0300

    :)

---
+++ cuban_pete.py
@@ -0,0 +1,20 @@
+def chick():
+    sing = "Chick-chicky-boom"
+    print(sing)
+    print(sing)
+
+song = ['They call me Cuban Pete','I\'am the king of the rumba beat','When I play the maracas I go','Yes, sir, I\'am Cuban Pete',
+'I\'m the crazy man down in the street','When I start to dance everything goes']
+
+k = 0
+while k < 6:
+    print(song[k])
+    k += 1
+    if k % 3 == 0:
+        chick()
+
+print("YAY!")
+print('      /\_/\         ')
+print('     | *w* |  /     ')
+print('      |   |  |      ')
+print('     |_|_|_|/       ')
